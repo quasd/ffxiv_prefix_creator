ea1430453d50ecd5c6dc16e9db030a12c18fc344
eki <eki.hur@gmail.com>
2022-06-24 18:57:04 +0300

    fix paths

--- ffxiv-run-stock.sh
+++ ffxiv-run-stock.sh
@@ -3,7 +3,7 @@
 cd "$(dirname "$0")"
 . ffxiv-env-setup.sh
 
-export xivlauncher="$WINEPREFIX/drive_c/FINAL FANTASY XIV - A Realm Reborn/boot/ffxivboot.exe"
+export xivlauncher="$WINEPREFIX/drive_c/Program Files (x86)\SquareEnix\FINAL FANTASY XIV - A Realm Reborn/boot/ffxivboot.exe"
 export actlauncher="$ACTFOLDER/Advanced Combat Tracker.exe"
 
 echo "Running: $WINE $xivlauncher"
